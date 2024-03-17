1ffd355f31147fb86307fca7f9ea18d104f7bcf3
Madhavmenon2022 <madhavmenon2022@vitbhopal.ac.in>
2024-03-17 11:06:29 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navibar from './components/Navibar/Navibar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navibar/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navibar/Navibar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './Navibar.css'
+import { assets } from '../../assets/assets'
+
+const Navibar = () => {
+
+    const [menu,setMenu] = useState("home");
+
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile app</li>
+        <li>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot"></div>
+          </div>
+          <button>sign in</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navibar
